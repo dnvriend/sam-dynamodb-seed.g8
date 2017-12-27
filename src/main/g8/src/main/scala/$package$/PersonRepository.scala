1917de04fb9b5be01366e71e5e3c86f9ca86383c
Dennis Vriend <dnvriend@gmail.com>
2017-12-27 13:46:17 +0100

    new interface

--- src/main/g8/src/main/scala/$package$/PersonRepository.scala
+++ src/main/g8/src/main/scala/$package$/PersonRepository.scala
@@ -1,21 +1,24 @@
 package $package$
 
+import java.util.UUID
+
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
-import com.github.dnvriend.dynamodb.repo.{JsonRepository, JsonRepositoryApiGatewayHandler}
 import com.github.dnvriend.lambda._
 import com.github.dnvriend.lambda.annotation.HttpHandler
+import com.github.dnvriend.repo.JsonRepository
 import play.api.libs.json.{Json, _}
 
 object Person {
   implicit val format: OFormat[Person] = Json.format[Person]
 }
+
 final case class Person(name: String, id: Option[String] = None)
 
 @HttpHandler(path = "/person", method = "put")
-class CreatePerson extends JsonRepositoryApiGatewayHandler[Person]("people") {
-  override def handle(person: Option[Person], repo: JsonRepository, request: HttpRequest, ctx: SamContext): HttpResponse = {
+class CreatePerson extends JsonDynamoRepoApiGatewayHandler[Person]("people") {
+  override def handle(person: Option[Person], pathParams: Map[String, String], requestParams: Map[String, String], request: HttpRequest, ctx: SamContext, repo: JsonRepository): HttpResponse = {
     person.fold(HttpResponse.validationError.withBody(Json.toJson("Could not deserialize person"))) { person =>
-      val id: String = repo.id
+      val id: String = UUID.randomUUID().toString
       repo.put(id, person)
       HttpResponse.ok.withBody(Json.toJson(person.copy(id = Option(id))))
     }
@@ -23,47 +26,45 @@ class CreatePerson extends JsonRepositoryApiGatewayHandler[Person]("people") {
 }
 
 @HttpHandler(path = "/person/{id}", method = "get")
-class ReadPerson extends JsonRepositoryApiGatewayHandler[Person]("people") {
-  override def handle(person: Option[Person], repo: JsonRepository, request: HttpRequest, ctx: SamContext): HttpResponse = {
-    request.pathParamsOpt[Map[String, String]].getOrElse(Map.empty).get("id")
-      .fold(HttpResponse.notFound.withBody(Json.toJson("No id found in path")))(id => {
-        repo.find[Person](id).fold(HttpResponse.validationError.withBody(Json.toJson("No person for id " + id))) { person =>
-          HttpResponse.ok.withBody(Json.toJson(person))
-        }
-      })
+class ReadPerson extends JsonDynamoRepoApiGatewayHandler[Person]("people") {
+  override def handle(person: Option[Person], pathParams: Map[String, String], requestParams: Map[String, String], request: HttpRequest, ctx: SamContext, repo: JsonRepository): HttpResponse = {
+    pathParams.get("id").fold(HttpResponse.notFound.withBody(Json.toJson("No id found in path")))(id => {
+      repo.find[Person](id).fold(HttpResponse.validationError.withBody(Json.toJson("No person for id " + id))) { person =>
+        HttpResponse.ok.withBody(Json.toJson(person))
+      }
+    })
   }
 }
 
 @HttpHandler(path = "/person/{id}", method = "post")
-class UpdatePerson extends JsonRepositoryApiGatewayHandler[Person]("people") {
-  override def handle(personOpt: Option[Person], repo: JsonRepository, request: HttpRequest, ctx: SamContext): HttpResponse = {
+class UpdatePerson extends JsonDynamoRepoApiGatewayHandler[Person]("people") {
+  override def handle(personOpt: Option[Person], pathParams: Map[String, String], requestParams: Map[String, String], request: HttpRequest, ctx: SamContext, repo: JsonRepository): HttpResponse = {
     val personAndId = for {
-      id <- request.pathParamsOpt[Map[String, String]].getOrElse(Map.empty).get("id")
+      id <- pathParams.get("id")
       person <- personOpt
     } yield (id, person)
 
     personAndId.fold(HttpResponse.validationError.withBody(Json.toJson("Could not deserialize person or id"))) {
       case (id, person) =>
-      repo.update(id, person)
-      HttpResponse.ok.withBody(Json.toJson(person.copy(id = Option(id))))
+        repo.update(id, person)
+        HttpResponse.ok.withBody(Json.toJson(person.copy(id = Option(id))))
     }
   }
 }
 
 @HttpHandler(path = "/person/{id}", method = "delete")
-class DeletePerson extends JsonRepositoryApiGatewayHandler[Person]("people") with RequestStreamHandler {
-  override def handle(person: Option[Person], repo: JsonRepository, request: HttpRequest, ctx: SamContext): HttpResponse = {
-    request.pathParamsOpt[Map[String, String]].getOrElse(Map.empty).get("id")
-      .fold(HttpResponse.notFound.withBody(Json.toJson("No id found in path")))(id => {
-        repo.delete(id)
-        HttpResponse.ok
-      })
+class DeletePerson extends JsonDynamoRepoApiGatewayHandler[Person]("people") with RequestStreamHandler {
+  override def handle(personOpt: Option[Person], pathParams: Map[String, String], requestParams: Map[String, String], request: HttpRequest, ctx: SamContext, repo: JsonRepository): HttpResponse = {
+    pathParams.get("id").fold(HttpResponse.notFound.withBody(Json.toJson("No id found in path")))(id => {
+      repo.delete(id)
+      HttpResponse.ok
+    })
   }
 }
 
 @HttpHandler(path = "/person", method = "get")
-class ListPersons extends JsonRepositoryApiGatewayHandler[Person]("people") {
-  override def handle(person: Option[Person], repo: JsonRepository, request: HttpRequest, ctx: SamContext): HttpResponse = {
+class ListPersons extends JsonDynamoRepoApiGatewayHandler[Person]("people") {
+  override def handle(personOpt: Option[Person], pathParams: Map[String, String], requestParams: Map[String, String], request: HttpRequest, ctx: SamContext, repo: JsonRepository): HttpResponse = {
     val listOfPerson: List[(String, Person)] = repo.list[Person]()
     val response: List[JsValue] = listOfPerson.map { case (id, p) => Json.obj("id" -> id) ++ Json.toJsObject(p) }
     HttpResponse.ok.withBody(Json.toJson(response))
